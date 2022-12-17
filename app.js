13c73e570e410e8d4307b9b27f852a926a8b7295
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2022-12-18 01:08:33 +0530

    initial setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const dbURL = process.env.DB_URL || "mongodb://localhost:27017/flights"
+mongoose.connect(dbURL)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+})
